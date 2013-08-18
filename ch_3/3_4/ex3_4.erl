5309261cf7358e253773bdfbc8d0b896370358b7
Igor Kuznetsov <igkuznetsov@gmail.com>
2013-08-18 11:23:10 +0400

    Excersize 4

--- ch_3/3_4/ex3_4.erl
+++ ch_3/3_4/ex3_4.erl
@@ -1,10 +1,19 @@
 -module(ex3_4).
--export([new/0, destroy/1, write/3]).
+-export([new/0, destroy/1, write/3, read/2]).
 
 new() -> [].
 destroy(_Db) -> ok.
 
 write(Key, Value, []) ->
   [{Key, Value}];
+write(Key, Value, [{Key, _}|Tail]) ->
+  [{Key, Value} | Tail];
 write(Key, Value, _Db) ->
   [{Key, Value} | _Db].
+
+read(_, []) ->
+  {error, instance};
+read(Key, [{Key, Value} | _]) ->
+  Value;
+read(Key, [_|Tail]) ->
+  read(Key, Tail).
