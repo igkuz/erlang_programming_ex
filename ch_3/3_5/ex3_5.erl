55dd13531435039b20c77082426c1a4fc7fc918f
Igor Kuznetsov <igkuznetsov@gmail.com>
2013-08-18 15:52:12 +0400

    Added some functions form ex3_5

--- ch_3/3_5/ex3_5.erl
+++ ch_3/3_5/ex3_5.erl
@@ -1,7 +1,7 @@
 -module(ex3_5).
--export([filter/2]).
+-export([filter/2, reverse/1, concatenate/1]).
 
-% Excersize 3 part 5
+% Excersize 3 part 5 filter function
 
 filter(List, Boundary) -> filter_list(List, Boundary).
 
@@ -11,3 +11,21 @@ filter_list([H|T], Boundary) when Boundary >= H ->
   [H| filter_list(T, Boundary)];
 filter_list([_|T], Boundary) ->
   filter_list(T, Boundary).
+
+% Excersize 3 part 5 reverse function
+
+reverse(List) -> reverse_acc(List, []).
+
+reverse_acc([], Acc) ->
+  Acc;
+reverse_acc([H | T], Acc) ->
+  reverse_acc(T, [H | Acc]).
+
+% Excersize 3 part 5 concatenate func
+
+concatenate(List) -> conc_acc(List, []).
+
+conc_acc([H | T], Acc) ->
+  conc_acc(T, Acc ++ H);
+conc_acc([], Acc) ->
+  Acc.
